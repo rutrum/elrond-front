e54288b7a66f1856432630116a829d553fc53f96
David Purdum <purdum41@gmail.com>
2018-07-16 19:47:08 -0400

    Add working template

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'main',
+        data() {
+            return {
+                title: "Project Elrond"
+            }
+        }
+    }
+</script>
+
+<style>
+    h1 {
+        text-align: center;
+        font-size: 40px;
+    }
+</style>
